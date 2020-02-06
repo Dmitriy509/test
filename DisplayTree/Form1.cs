715f800d90bfc15aaff43435dc02a4839d235709
Dmitriy <dmv.kirillov@gmail.com>
2020-02-06 11:13:58 +0300

    Create bd

---
+++ DisplayTree/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (catalogDb db = new catalogDb())
+            {
+
+                db.CatalogSet.Add(new Catalog { Id = 1, Description = "volvo description", Name = "VOLVO" });
+                db.CatalogSet.Add(new Catalog { Id = 2, Description = "ER description", Name = "ER" });
+                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 1, CatalogId = 1, Description = "КПП asdfasf", Name = "КПП", Url = "sdf" });
+                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 2, CatalogId = 2, Description = "Двигатель asdfasf", Name = "Двигатель", Url = "aaaa" });
+                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 3, CatalogId = 2, Description = "КПП sdf", Name = "КПП", Url = "cvvv" });
+                db.CatalogModels.Add(new Catalog_model { Id = 1, Catalog_aggregateId = 1, Model = "A635", Description = "aaa", Url = "asdf" });
+                db.CatalogModels.Add(new Catalog_model { Id = 2, Catalog_aggregateId = 2, Model = "M4566", Description = "bbb", Url = "asdf" });
+                db.CatalogModels.Add(new Catalog_model { Id = 3, Catalog_aggregateId = 2, Model = "FG4511", Description = "ccc", Url = "ccacc" });
+                db.CatalogModels.Add(new Catalog_model { Id = 4, Catalog_aggregateId = 3, Model = "T45459", Description = "ddd", Url = "cccc" });
+                db.CatalogModels.Add(new Catalog_model { Id = 5, Catalog_aggregateId = 2, Model = "A77", Description = "iii", Url = "cccc" });
+                db.SaveChanges();
+
+
+            }
+        }
+    }
+}
---
+++ DisplayTree/catalogDb.cs
@@ -0,0 +1,76 @@
+﻿namespace DisplayTree
+{
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+    public class catalogDb : DbContext
+    {
+
+        public catalogDb()
+            : base("name=catalogDb")
+        {
+        }
+
+        public DbSet<Catalog> CatalogSet { get; set; }
+        public DbSet<Catalog_aggregate> CatalogAggrSet { get; set; }
+        public DbSet<Catalog_model> CatalogModels { get; set; }
+        public DbSet<Catalog_level> CatalogLevelSet { get; set; }
+
+    }
+
+    public class Catalog
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Catalog_aggregate> Catalog_aggregates { get; set; }
+        public Catalog()
+        {
+            Catalog_aggregates = new List<Catalog_aggregate>();
+        }
+    }
+    public class Catalog_aggregate
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public int CatalogId { get; set; }
+        public virtual Catalog Catalog { get; set; }
+
+        public virtual ICollection<Catalog_model> Catalog_models { get; set; }
+        public Catalog_aggregate()
+        {
+            Catalog_models = new List<Catalog_model>();
+        }
+
+    }
+
+    public class Catalog_model
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public virtual int Catalog_aggregateId { get; set; }
+        public virtual Catalog_aggregate catalog_aggr { get; set; }
+    }
+
+    public class Catalog_level
+    {
+        public int Id { get; set; }
+        public int? Parent_Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
