3197339db9add691700947f0b618f5e3695ce0c6
Dmitriy <dmv.kirillov@gmail.com>
2020-02-06 11:59:39 +0300

    fill table Catalog_level

--- DisplayTree/Form1.Designer.cs
+++ DisplayTree/Form1.Designer.cs
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.catalogTreeView = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // catalogTreeView
+            // 
+            this.catalogTreeView.Location = new System.Drawing.Point(12, 12);
+            this.catalogTreeView.Name = "catalogTreeView";
+            this.catalogTreeView.Size = new System.Drawing.Size(203, 327);
+            this.catalogTreeView.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Заполняем catalog_level";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(555, 435);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.catalogTreeView);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView catalogTreeView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DisplayTree/Form1.cs
+++ DisplayTree/Form1.cs
@@ -15,23 +15,101 @@ namespace DisplayTree
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
             using (catalogDb db = new catalogDb())
             {
 
-                db.CatalogSet.Add(new Catalog { Id = 1, Description = "volvo description", Name = "VOLVO" });
-                db.CatalogSet.Add(new Catalog { Id = 2, Description = "ER description", Name = "ER" });
-                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 1, CatalogId = 1, Description = "КПП asdfasf", Name = "КПП", Url = "sdf" });
-                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 2, CatalogId = 2, Description = "Двигатель asdfasf", Name = "Двигатель", Url = "aaaa" });
-                db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 3, CatalogId = 2, Description = "КПП sdf", Name = "КПП", Url = "cvvv" });
-                db.CatalogModels.Add(new Catalog_model { Id = 1, Catalog_aggregateId = 1, Model = "A635", Description = "aaa", Url = "asdf" });
-                db.CatalogModels.Add(new Catalog_model { Id = 2, Catalog_aggregateId = 2, Model = "M4566", Description = "bbb", Url = "asdf" });
-                db.CatalogModels.Add(new Catalog_model { Id = 3, Catalog_aggregateId = 2, Model = "FG4511", Description = "ccc", Url = "ccacc" });
-                db.CatalogModels.Add(new Catalog_model { Id = 4, Catalog_aggregateId = 3, Model = "T45459", Description = "ddd", Url = "cccc" });
-                db.CatalogModels.Add(new Catalog_model { Id = 5, Catalog_aggregateId = 2, Model = "A77", Description = "iii", Url = "cccc" });
-                db.SaveChanges();
+             //   db.Database.ExecuteSqlCommand("DELETE FROM Catalogs");
+              //db.Database.ExecuteSqlCommand("DELETE FROM Catalog_model");
+              //db.Database.ExecuteSqlCommand("DELETE FROM Catalog_aggregate");
+              //db.Database.ExecuteSqlCommand("DELETE FROM Catalog_level");
+            }
 
+            using (catalogDb db = new catalogDb())
+            {
+
+                if (!db.CatalogSet.Any())
+                {
+
+                    db.CatalogSet.Add(new Catalog { Id = 1, Description = "volvo description", Name = "VOLVO" });
+                    db.CatalogSet.Add(new Catalog { Id = 2, Description = "ER description", Name = "ER" });
+                    db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 1, CatalogId = 1, Description = "КПП asdfasf", Name = "КПП", Url = "sdf" });
+                    db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 2, CatalogId = 2, Description = "Двигатель asdfasf", Name = "Двигатель", Url = "aaaa" });
+                    db.CatalogAggrSet.Add(new Catalog_aggregate { Id = 3, CatalogId = 2, Description = "КПП sdf", Name = "КПП", Url = "cvvv" });
+                    db.CatalogModels.Add(new Catalog_model { Id = 1, Catalog_aggregateId = 1, Model = "A635", Description = "aaa", Url = "asdf" });
+                    db.CatalogModels.Add(new Catalog_model { Id = 2, Catalog_aggregateId = 2, Model = "M4566", Description = "bbb", Url = "asdf" });
+                    db.CatalogModels.Add(new Catalog_model { Id = 3, Catalog_aggregateId = 2, Model = "FG4511", Description = "ccc", Url = "ccacc" });
+                    db.CatalogModels.Add(new Catalog_model { Id = 4, Catalog_aggregateId = 3, Model = "T45459", Description = "ddd", Url = "cccc" });
+                    db.CatalogModels.Add(new Catalog_model { Id = 5, Catalog_aggregateId = 2, Model = "A77", Description = "iii", Url = "cccc" });
+                    db.SaveChanges();
+                }
+            }
+        }
+
+
+        Catalog_level addRow(int? parent_id, string name, string descr, catalogDb db)
+        {
+            Catalog_level newItem = new Catalog_level { Parent_Id = null, Name = name, Description = descr };
+            db.CatalogLevelSet.Add(newItem);
+            db.SaveChanges();
+            return newItem;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (catalogDb db = new catalogDb())
+            {
+                if (!db.CatalogLevelSet.Any())
+                {
+                    var catalog = db.CatalogSet.ToList();
+                    foreach (var item in catalog)
+                    {
+                        Catalog_level newItem = addRow(null, item.Name, item.Description, db);
+                        int newCatalogId = newItem.Id;
+
+                        var catalog_aggr = db.CatalogAggrSet.Where(u => u.CatalogId == item.Id).ToList();
+
+                        foreach (var item2 in catalog_aggr)
+                        {
+
+                            newItem = addRow(newCatalogId, item2.Name, item2.Description, db);
+                            int newCatalogAggrId = newItem.Id;
+                            var catalog_model = db.CatalogModels.Where(u => u.Catalog_aggregateId == item2.Id).ToList();
+
+
+                            foreach (var item3 in catalog_model)
+                            {
+                                newItem = addRow(newCatalogAggrId, item3.Model, item3.Description, db);
+
+                            }
+
+                        }
+
+                    }
+                }
 
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
